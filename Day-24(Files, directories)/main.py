e5b16ca37d42b81807ae800de6e8bcabd6098c5d
Sufyan <sufianjellani@gmail.com>
2023-09-26 19:19:44 +0500

    This is my Initial Commit

---
+++ Day-24(Files, directories)/main.py
@@ -0,0 +1,8 @@
+# with open("file.txt") as file:
+#     content = file.read()
+#     print(content)
+
+with open("nfile.txt", mode="w") as file:
+    file.write("\nPeople should fear from my wrath.")
+    # print(content)
+
